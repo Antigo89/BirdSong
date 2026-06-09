437580f1d9837900555c4dd52a9f4c40c8cdb6ba
Antigo89 <antigo1989@gmail.com>
2026-06-09 03:27:06 +0300

    chore: added key

--- BirdSong_arduino/BirdSong_arduino.ino
+++ BirdSong_arduino/BirdSong_arduino.ino
@@ -1,77 +1,64 @@
 #include <SPI.h>
 #include <SD.h>
 #include <TMRpcm.h>
+#define TESTMODE
+#define LED_ACTIVATE
+
+#define KEY_PIN 2
+#define AUDIO_PIN 9 //9 or 10
 
 TMRpcm tmrpcm;
-Sd2Card card;
-SdVolume volume;
-SdFile root;
+volatile bool playmode = false;
 
-void setup() {
-  tmrpcm.speakerPin = 9; // Для Arduino Nano/Uno это строго пин 9!
-  
-  Serial.begin(9600);
-  
-  if (!SD.begin(4)) {
-    Serial.println("Ошибка инициализации SD-карты!");
+void keyIsr(void){
+  if(playmode){
+    playmode = false;
   }else{
-    Serial.println("SD init");
-  }
-  if (!card.init(SPI_HALF_SPEED, 4)) {
-    Serial.println("initialization failed. Things to check:");
-    Serial.println("* is a card inserted?");
-    Serial.println("* is your wiring correct?");
-    Serial.println("* did you change the chipSelect pin to match your shield or module?");
-    Serial.println("Note: press reset button on the board and reopen this Serial Monitor after fixing your issue!");
-    while (1);
-  } else {
-    Serial.println("Wiring is correct and a card is present.");
-  }
-  Serial.println();
-  Serial.print("Card type:         ");
-  switch (card.type()) {
-    case SD_CARD_TYPE_SD1:
-      Serial.println("SD1");
-      break;
-    case SD_CARD_TYPE_SD2:
-      Serial.println("SD2");
-      break;
-    case SD_CARD_TYPE_SDHC:
-      Serial.println("SDHC");
-      break;
-    default:
-      Serial.println("Unknown");
-  }
-  if (!volume.init(card)) {
-    Serial.println("Could not find FAT16/FAT32 partition.\nMake sure you've formatted the card");
-    while (1);
-  }
-
-  Serial.print("Clusters:          ");
-  uint32_t volumesize;
-  Serial.print("Volume type is:    FAT");
-  Serial.println(volume.fatType(), DEC);
-  volumesize = volume.blocksPerCluster();    // clusters are collections of blocks
-  volumesize *= volume.clusterCount();       // we'll have a lot of clusters
-  volumesize /= 2;                           // SD card blocks are always 512 bytes (2 blocks are 1 KB)
-  Serial.print("Volume size (MB):  ");
-  volumesize /= 1024;
-  Serial.println(volumesize);
-  Serial.println("\nFiles found on the card (name, date and size in bytes): ");
-  root.openRoot(volume);
-  root.ls(LS_R | LS_DATE | LS_SIZE);
-  root.close();
-
-  if (SD.exists("canary.wav")) {
-    Serial.println("Play canary.wav");
-    tmrpcm.volume(5);      // Громкость от 0 до 7s
-    tmrpcm.play("canary.wav");   // Воспроизведение файла
-  }else{
-    Serial.println("canary.wav is not exist");
+    playmode = true;
   }
 }
 
+void setup() {
+  tmrpcm.speakerPin = AUDIO_PIN;
+  #ifdef TESTMODE
+  Serial.begin(9600);
+  #endif
+  pinMode(KEY_PIN, INPUT_PULLUP);
+  attachInterrupt(0, keyIsr, FALLING);
+
+  if (!SD.begin(4)) {
+    #ifdef TESTMODE
+    Serial.println("Ошибка инициализации SD-карты!");
+    #endif
+  }else{
+    #ifdef TESTMODE
+    Serial.println("SD init");
+    #endif
+  }
+  tmrpcm.quality(1);
+  tmrpcm.volume(5);
+    
+}
+
 void loop() {
+  if(playmode){
+    if(!tmrpcm.isPlaying()){
+      digitalWrite(AUDIO_PIN, LOW);
+      if (SD.exists("canary.wav")) {
+        #ifdef TESTMODE
+        Serial.println("Play canary.wav");
+        #endif
+        
+        tmrpcm.play("canary.wav");
+      }
+    }
+  }else{
     tmrpcm.stopPlayback();
-  // Код программы
+    digitalWrite(AUDIO_PIN, LOW);
+  }
+  #ifdef TESTMODE
+  Serial.print("playmode = ");
+  Serial.println(playmode);
+  #endif
+  _delay_ms(1000);
 }
